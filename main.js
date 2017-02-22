c7af1b24484414e9019414e45b326ce90f94c6f5
Sanyyouisf <sanymamdoh@yahoo.com>
2017-02-21 18:56:27 -0600

    adding function to the button

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("is working");
+
+var plantsButton = document.getElementById("showPlants"); 
+plantsButton.addEventListener("click", function(){
+console.log("button is pressed");
+
+});
